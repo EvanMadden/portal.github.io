d592eec2572ed48f5617c2ca21f0c2fead63624c
EvanMadden <64569018+EvanMadden@users.noreply.github.com>
2020-04-29 22:37:35 -0400

    first commit

---
+++ Assets/app.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function ($) {
+
+    $('#checkbox').change(function(){
+      setInterval(function () {
+          moveRight();
+      }, 3000);
+    });
+    
+      var slideCount = $('#slider ul li').length;
+      var slideWidth = '1000px';
+      var slideHeight = '700px';
+      var sliderUlWidth = slideCount * slideWidth;
+      
+      $('#slider').css({ width: slideWidth, height: slideHeight });
+      
+      $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+      
+      $('#slider ul li:last-child').prependTo('#slider ul');
+  
+      function moveLeft() {
+          $('#slider ul').animate({
+              left: + slideWidth
+          }, 200, function () {
+              $('#slider ul li:last-child').prependTo('#slider ul');
+              $('#slider ul').css('left', '');
+          });
+      };
+  
+      function moveRight() {
+          $('#slider ul').animate({
+              left: - slideWidth
+          }, 200, function () {
+              $('#slider ul li:first-child').appendTo('#slider ul');
+              $('#slider ul').css('left', '');
+          });
+      };
+  
+      $('a.PrevBtn').click(function () {
+          moveLeft();
+      });
+  
+      $('a.NextBtn').click(function () {
+          moveRight();
+      });
+  
+  });    
+  
